f2260c766123568ecbfede1a39e319af0b714798
younggyung <melondumpling01@gmail.com>
2023-04-27 13:38:08 +0900

    230427 넘버게임

--- number_guess_game/main.js
+++ number_guess_game/main.js
@@ -3,7 +3,7 @@
 // 만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
 // 랜덤번호 < 유저번호 일때, DOWN
 // 랜덤번호> 유저번호 일때, UP
-// Reset 버튼 = 리셋
+// Reset 버튼 = 리셋 
 // 5번의 기회를 다쓰면 게임끝. (더이상 추측 불가, 버튼 disable)
 // 유저가 1~100 범위 밖 숫자를 입력하면 알려준다. 기회는 그대로
 // 유저가 이미 입력한 숫자를 또 입력하면 알려준다. 기회는 그대로
@@ -12,8 +12,14 @@ let computerNum = 0;
 let playButton = document.getElementById("play");
 let userInput = document.getElementById("user-input"); //태그 통째로
 let result = document.getElementById("result-area");
+let resetButton = document.getElementById("reset");
+let chance = document.getElementById("chance");
+let chanceNum = 5;
 
+//클릭이벤트 지정시, 함수의 '이름'만 넣기
 playButton.addEventListener("click",play);
+resetButton.addEventListener("click",reset);
+
 function pickRandomNum(){
     computerNum = Math.floor(Math.random()*100)+1;
     console.log("정답",computerNum);
@@ -21,6 +27,10 @@ function pickRandomNum(){
 
 function play(){
    let userValue = userInput.value; //태그에서 딱 value만 가져옴
+    if(userValue >100 || userValue <=0){
+        alert("1에서 100 사이의 숫자를 입력해주세요!")
+        return false;
+    } 
     if(userValue>computerNum){
         console.log("DOWN!!");
         result.textContent = "DOWN!!"
@@ -29,8 +39,21 @@ function play(){
     }else{
         result.textContent = "정답입니다^^"
     }
-   console.log(userInput);
+    chanceNum -= 1;
+    chance.textContent = chanceNum;
+    if(chanceNum === 0){
+       playButton.disabled = true;
+    }
+    console.log(chanceNum);
+    
 }
+
+function reset(){
+    console.log("리셋합니다")
+    pickRandomNum();
+    result.textContent = "LET's GO!"
+}
+
 
 pickRandomNum();
 //Math.random()는 0부터 1사이의 숫자 호출
