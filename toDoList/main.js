398f92932578a481821e2321d93abd048f4018b9
younggyung <melondumpling01@gmail.com>
2023-05-04 02:17:19 +0900

    230503 TIL

--- toDoList/main.js
+++ toDoList/main.js
@@ -7,47 +7,58 @@
 //메뉴탭을 누를때 마다 언더바 이동
 //각각의 상태에 맞게 아이템 표시
 //전체탭을 누르면 다시 전체 아이템으로 돌아옴
+let filteredList=[];
+let mode ='all';
 let taskList = [];
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
 addButton.addEventListener("click",addTask);
+let tabs = document.querySelectorAll(".task-tabs div")
+for(let i = 1; i<tabs.length;i++){
+  tabs[i].addEventListener("click",function(event){filter(event)});
+}
 
 function addTask(){
   let task={
     taskContent:taskInput.value, 
     isComplete:false,
     id:randomIDGenerator()
-    
           };
   taskList.push(task);
   console.log(taskList);
   render();
 }
 
-
 function render(){
-document.getElementById("underLine").style["margin-left"]=0;
+  let list =[];
+  if(mode =='all'){
+    list = taskList;
+  }else if(mode =='inprogress'){
+    list = filteredList;
+  }else if(mode =='done'){
+    list = filteredList;
+  }
+
   let resultHTML ='';
-  for(let i = 0 ; i<taskList.length; i++){
-    if(taskList[i].isComplete == true){
+  for(let i = 0 ; i<list.length; i++){
+    if(list[i].isComplete == true){
       resultHTML += `<div class="task">
-      <div class="task-done">${taskList[i].taskContent}</div>
+      <div class="task-done">${list[i].taskContent}</div>
         <div class="buttons">
-          <button onclick="check('${taskList[i].id}')"><img src="images/check.png"></button>
-          <button onclick="deleteTask('${taskList[i].id}')"><img src="images/trash.png"></button>
+          <button onclick="check('${list[i].id}')"><img src="images/check.png"></button>
+          <button onclick="deleteTask('${list[i].id}')"><img src="images/trash.png"></button>
           </div>
         </div>`;
     }else{
     resultHTML += `<div class="task">
-    <div>${taskList[i].taskContent}</div>
+    <div>${list[i].taskContent}</div>
       <div class="buttons">
-        <button onclick="check('${taskList[i].id}')"><img src="images/check.png"></button>
-        <button onclick="deleteTask('${taskList[i].id}')"><img src="images/trash.png"></button>
+        <button onclick="check('${list[i].id}')"><img src="images/check.png"></button>
+        <button onclick="deleteTask('${list[i].id}')"><img src="images/trash.png"></button>
       </div>
     </div>`
   }
 }
-//뿌려주기
   document.getElementById("task-board").innerHTML = resultHTML;
 }
 
@@ -71,6 +82,32 @@ function deleteTask(id){
   render();
 }
 
+
+function filter(event){
+  mode = event.target.id;
+  filteredList = [];
+  document.getElementById("underLine").style.width = event.target.offsetWidth + "px";
+  document.getElementById("underLine").style.top = event.target.offsetTop + event.target.offsetHeight + "px";
+  document.getElementById("underLine").style.left = event.target.offsetLeft + "px";
+  
+  if(mode == 'all'){
+    render();
+  }else if(mode =='inprogress'){
+    for(let i = 0; i<taskList.length;i++){
+      if(taskList[i].isComplete == false){
+        filteredList.push(taskList[i]);
+      }
+    }
+    render();
+  }else if(mode =='done'){
+    for(let i=0; i<taskList.length; i++){
+      if(taskList[i].isComplete == true){
+        filteredList.push(taskList[i]);
+      }
+    }
+  }render();
+}
+
 //엔터키로 추가
 taskInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
@@ -81,45 +118,5 @@ taskInput.addEventListener('keydown', (event) => {
 //랜덤한 id값 생성, check 버튼을 눌렀을때 어떤 오브젝트인지 식별하기 위해서 !
 function randomIDGenerator(){
   return '_' + Math.random().toString(34).substring(2,9);
-}
-
-function ShowInProgress(){
-  //언더바 이동
-  document.getElementById("underLine").outerHTML = `<div id="underLine" style="margin-left: 70px;"></div>`;
-
-  //필터링
-  let resultHTML ='';
-  for(let i = 0 ; i<taskList.length; i++){
-    if(taskList[i].isComplete == false){
-      resultHTML += `<div class="task">
-      <div class="task">${taskList[i].taskContent}</div>
-        <div class="buttons">
-          <button onclick="check('${taskList[i].id}')"><img src="images/check.png"></button>
-          <button onclick="deleteTask('${taskList[i].id}')"><img src="images/trash.png"></button>
-          </div>
-        </div>`;
-    }
-  }
-  document.getElementById("task-board").innerHTML = resultHTML;
-}
-
-
-function ShowDone(){
-  //언더바 이동
-document.getElementById("underLine").outerHTML = `<div id="underLine" style="margin-left: 140px;"></div>`;
-//필터링
-  let resultHTML ='';
-  for(let i = 0 ; i<taskList.length; i++){
-    if(taskList[i].isComplete == true){
-      resultHTML += `<div class="task">
-      <div class="task-done">${taskList[i].taskContent}</div>
-        <div class="buttons">
-          <button onclick="check('${taskList[i].id}')"><img src="images/check.png"></button>
-          <button onclick="deleteTask('${taskList[i].id}')"><img src="images/trash.png"></button>
-          </div>
-        </div>`;
-    }
-  }
-  document.getElementById("task-board").innerHTML = resultHTML;
 }
 
