d83bef312bf115ed662f413ff15f2adb0639cb2c
younggyung <melondumpling01@gmail.com>
2023-05-20 03:11:03 +0900

    commit for laptop work

--- REACT/starting-project/src/components/PostList.jsx
+++ REACT/starting-project/src/components/PostList.jsx
@@ -2,22 +2,32 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function PostList({ isPosting, onStopPosting }) {
-  fetch('http://localhost:8080/posts').then(response => response.json()).then(data=>data.posts);
-
   const [posts, setPosts] = useState([]);
 
-  function addPostHandler(postData) {
-    fetch('http://localhost:8080/posts'),{
-      method: 'POST',
-      body : JSON.stringify(postData),
-      headers : {
-        'Content-Type' : 'application/json'
-      }
-    };
+  // fetch('http://localhost:8080/posts').then(response => response.json()).then(data=>data.posts){
+  //   setPosts(data.posts);
+  // }; 이렇게 되면 setPosts가 컴포넌트를 계속 재실행하게되어 fetch가 계속 실행되고 무한루프 발생 ====> 그럼 어떡해? ==> useEffect 사용
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+    fetchPosts();
+  }, []);
+
+  function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
@@ -30,11 +40,9 @@ function PostList({ isPosting, onStopPosting }) {
       )}
       {posts.length > 0 && (
         <ul className={classes.post}>
-          {posts.map((post) => 
-              <Post key={Math.random()} author={post.author} body=
-              {post.body} />
-            )
-          }
+          {posts.map((post) => (
+            <Post key={Math.random()} author={post.author} body={post.body} />
+          ))}
         </ul>
       )}
       {posts.length === 0 && (
